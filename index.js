1b3f7eb2d915be7cd29b10c38c22de3d0b6fd8c1
Irene Gitau <irene.gitau@strathmore.edu>
2023-09-21 16:05:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages; 
+  }
+  const names = ["Alice", "Bob", "Charlie"];
+const event = "birthday";
+const messages = writeCards(names, event);
+
+console.log(messages);
+function countDown(number) { for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+  }
+  module.exports = {
+    countDown,
+  };
